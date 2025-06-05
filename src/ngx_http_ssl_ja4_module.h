259a7fd5694be70c88c01b2d752e6de1b1133b1c
liboabo <libo@vks.su>
2025-06-05 14:37:06 +0300

    Fixed incorrect tls version detection

--- src/ngx_http_ssl_ja4_module.h
+++ src/ngx_http_ssl_ja4_module.h
@@ -274,7 +274,7 @@ ngx_ssl_ja4_detail_print(ngx_pool_t *pool, ngx_ssl_ja4_t *ja4)
 
     /* Version */
     ngx_log_debug1(NGX_LOG_DEBUG_EVENT,
-                   pool->log, 0, "ssl_ja4: Version:  %d\n", ja4->version);
+                   pool->log, 0, "ssl_ja4: Version:  %s", ja4->version);
 
     /* Ciphers */
     ngx_log_debug1(NGX_LOG_DEBUG_EVENT,
@@ -368,7 +368,7 @@ ngx_ssl_ja4_detail_print(ngx_pool_t *pool, ngx_ssl_ja4_t *ja4)
     //                    ja4->alpn_first_value);
     // }
 }
-    
+
 #endif
 
 // FUNCTION PROTOTYPES
